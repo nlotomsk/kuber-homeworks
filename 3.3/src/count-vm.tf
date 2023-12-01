ff3f9d1cc5036ff48d2cb2306f6015e30e97e7ab
Slava <nlo_tomsk@mail.ru>
2023-12-01 16:43:07 +0700

    123

--- 3.3/src/count-vm.tf
+++ 3.3/src/count-vm.tf
@@ -1,5 +1,5 @@
 resource "yandex_compute_instance" "worknode" {
-  count       = 4
+  count       = 1
   name        = "${local.role.1}-0${count.index+1}"
   hostname    = "${local.role.1}-0${count.index+1}"
   platform_id = var.vm_worknode_instance_platform_id
